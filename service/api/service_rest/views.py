ab167f5a02bdeb46e366bac4956fbbed99ceb0fa
Kyle <bossertkt@gmail.com>
2023-10-23 15:46:36 -0700

    Created Models: Appointment, Technician, AutomobileVO, and encoders.

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
-
+from .models import Technician, AutomobileVO, Appointment
+from common.json import ModelEncoder
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 # Create your views here.
+
+class TechnicianListEncoer(ModelEncoder):
+    model = Technician
+    properties = [
+        "id",
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+class TechnicianDetailEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "id",
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+class AppointmentListEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "id",
+        "date_time",
+        "status",
+        "reason",
+        "customer",
+        "vin",
+        "technician",
+    ]
+    encoders = {
+        "technician": TechnicianDetailEncoder()
+    }
+
+class AppointmentDetiailEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "id",
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer",
+        "technician"
+    ]
+    encoders = {
+        "technician":TechnicianDetailEncoder(),
+    }
