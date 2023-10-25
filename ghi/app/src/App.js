2a09730594f7e0b3e687c9d9ee507e2dda0cfb24
Kyle <bossertkt@gmail.com>
2023-10-24 17:16:04 -0700

    Technician list and new tech form and delete button

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import TechnicianForm from './TechnicianForm';
+import TechniciansList from './TechniciansList';
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="technicians">
+            <Route index element={<TechniciansList />} />
+            <Route path="new" element={<TechnicianForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
